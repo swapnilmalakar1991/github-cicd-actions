33202af6da08e727bb17b38226f3213975f21e4a
swapnilmalakar1991 <swapnilmalakar.1991@gmail.com>
2023-04-17 20:03:53 +0530

    First commit

---
+++ src/main/java/com/globant/docker/github/cicd/actions/DockerGithubCicdActionsApplication.java
@@ -0,0 +1,13 @@
+package com.globant.docker.github.cicd.actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerGithubCicdActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerGithubCicdActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globant/docker/github/cicd/actions/DockerGithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.docker.github.cicd.actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerGithubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
